0a74e27adce33b54829c77edc91df4df9c5dd29e
Taylor Boren <taylorboren@teletech.com>
2014-12-04 13:23:51 -0700

    Get all users

---
+++ apps/SKE/public/javascripts/users.js
@@ -0,0 +1,18 @@
+var users = {
+	
+	getAll: function(i){
+		if(i < 0) return;
+		osapi.jive.core.get({
+	        "href": "/people?count=100&startIndex="+i.toString(),
+	        "v": "v3"
+	    }).execute(function(response){
+	        if(response.content.list.length > 0){
+	        	users.getAll(i + 100);
+	        	console.log(response);  
+	        }
+	        else{
+	        	users.getAll(-1);
+	        }
+	    });
+	}
+}
