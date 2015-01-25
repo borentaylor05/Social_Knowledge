3d7e4476147777ef826d72ef6ccbaf8aa0022416
Taylor Boren <taylorboren@teletech.com>
2015-01-25 09:13:35 -0700

    Testing on demo instance

--- apps/SKE/public/javascripts/fairfax/ng-fairfax.js
+++ apps/SKE/public/javascripts/fairfax/ng-fairfax.js
@@ -19,17 +19,20 @@ app.controller("Fairfax", ['$http', '$scope', function($http, $scope){
 	fx.view = "main";
 	fx.header = "Welcome to Fairfax";
 	fx.sideMenu = "care";
-	fx.people = [];
+	fx.rates = [];
+	fx.geos = [];
+	fx.mastheads = [];
 	
-	fx.select = function(tags){
+	fx.select = function(pub){
+		fx.selected = getPubTitle(pub);
+		fx.pubSelected = true;
 		osapi.jive.core.get({
-	        "href": "/search/people?filter=search("+tags+")",
+	        "href": "/search/contents?filter=tag("+pub+")",
 	        "v": "v3"
-	    }).execute(function(data){
-	    	data = user.responseCheck(data);
-	    	console.log(data);
-	    	fx.people = data.list;
-	    	$scope.$apply(fx.people);
+	    }).execute(function(resp){
+	    	data = user.responseCheck(resp);
+	    	divvyUp(resp.list)
+	    	util.adjustHeight();
 	    });
 	}
 
@@ -37,4 +40,56 @@ app.controller("Fairfax", ['$http', '$scope', function($http, $scope){
 		navigation.go(view);
 	}
 	
+	var divvyUp = function(docs, callback){
+		var tags;
+		if(docs){
+			for (var i = docs.length - 1; i >= 0; i--) {
+				tags = docs[i].tags;
+				if(tags.indexOf("fx-rates") >= 0)
+					fx.rates.push(docs[i]);
+				else if(tags.indexOf("fx-geos"))
+					fx.geos.push(docs[i]);
+				else if(tags.indexOf("fx-mastheads"))
+					fx.mastheads.push(docs[i]);
+			};
+			$scope.$apply(fx.geos);
+			$scope.$apply(fx.rates);
+			$scope.$apply(fx.mastheads);
+		}
+	}
+	var getPubTitle = function(pub){
+		switch(pub){
+			case "dompost":
+				return "Dominion Post";
+			break;
+			case "manawatu-standard":
+				return "Manawatu Standard";
+			break;
+			case "marlborough":
+				return "Marlborough Express";
+			break;
+			case "nelson-mail":
+				return "Neloson Mail";
+			break;
+			case "the-press":
+				return "The Press";
+			break;
+			case "southland-times":
+				return "Southland Times";
+			break;
+			case "smh":
+				return "Sydeney Morning Herald";
+			break;
+			case "taranaki":
+				return "Taranaki NZ";
+			break;
+			case "timaru-times":
+				return "Timaru Times";
+			break;
+			case "waikato-times":
+				return "Waikato Times";
+			break;
+		}
+	}
+
 }]);
